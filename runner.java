acc255131d381d4ee55bb955a27f98678b19023e
reshmapalani <48348519+reshmapalani@users.noreply.github.com>
2020-03-03 13:58:35 +0530

    Add files via upload

---
+++ runner.java
@@ -0,0 +1,14 @@
+class runner
+{
+	public static void main(String args[]) 
+	{
+
+        double kilometers = 14.0;
+        double miles = kilometers / 1.6;
+
+        double rate = (45.5 * 60.0 + 30.0) / (60.0 * 60.0);
+        double milesPerHour = miles / rate;
+
+        System.out.println(milesPerHour);
+	}
+}
